c2378163fa2dce4e0975cd383dc77178c2132814
tlsftlsftlsf <tlsftlsftlsf@sun.edu.pl>
2025-05-30 19:41:46 +0800

    Update Sliverkiss_sfsy.js

--- Sliverkiss_sfsy.js
+++ Sliverkiss_sfsy.js
@@ -7,7 +7,7 @@
 重写:打开小程序，进入我的页面，点击优惠券页面获取token。支持多账号。
 
 [Script]
-http-response ^https:\/\/mcs-mimp-web\.sf-express\.com\/mcs-mimp\/share\/weChat\/shareGiftReceiveRedirect.+ script-path=https://gitee.com/tlsfcn/tlsf/raw/master/sfsy.js, requires-body=true, timeout=60, tag=顺丰速运获取token
+http-response ^https:\/\/mcs-mimp-web\.sf-express\.com\/mcs-mimp\/share\/weChat\/shareGiftReceiveRedirect.+ script-path=https://raw.githubusercontent.com/tlsftlsftlsf/qinglong/refs/heads/main/Sliverkiss_sfsy.js, requires-body=true, timeout=60, tag=顺丰速运获取token
 
 [MITM]
 hostname = mcs-mimp-web.sf-express.com
